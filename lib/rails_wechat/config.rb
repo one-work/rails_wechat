27e09d606a98bf9c8f994cf70ff4d3b2bb0862ef
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-07 19:31:43 +0800

    auto commit

--- lib/rails_wechat/config.rb
+++ lib/rails_wechat/config.rb
@@ -1,52 +1,49 @@
 module RailsWechat
-  include ActiveSupport::Configurable
+  mattr_accessor :config, default: ActiveSupport::OrderedOptions.new
 
-  configure do |config|
-    config.email_domain = 'mail.work.design'
-    config.confirm = false
-    config.suite_id = ''
-    config.rules = ActiveSupport::OrderedOptions.new
-    config.rules.a = {
-      msg_type: 'event',
-      event: 'templatesendjobfinish',
-      proc: ->(request) {
-        r = Wechat::Notice.find_by(msg_id: request.raw_body['MsgID'])
-        r.update status: request.raw_body['Status']
-        Wechat::TextReply.new(value: 'SUCCESS')
-      }
+  config.email_domain = 'mail.work.design'
+  config.confirm = false
+  config.suite_id = ''
+  config.rules = ActiveSupport::OrderedOptions.new
+  config.rules.a = {
+    msg_type: 'event',
+    event: 'templatesendjobfinish',
+    proc: ->(request) {
+      r = Wechat::Notice.find_by(msg_id: request.raw_body['MsgID'])
+      r.update status: request.raw_body['Status']
+      Wechat::TextReply.new(value: 'SUCCESS')
     }
-    config.rules.b = {
-      msg_type: 'event',
-      event: 'click',
-      body: 'bind',
-      proc: ->(request) { request.reply_for_user }
+  }
+  config.rules.b = {
+    msg_type: 'event',
+    event: 'click',
+    body: 'bind',
+    proc: ->(request) { request.reply_for_user }
+  }
+  config.rules.b2 = {
+    msg_type: 'event',
+    event: ['subscribe', 'scan'],
+    body: /^invite_(member|by)_/,
+    proc: ->(request) { request.reply_for_blank_info }
+  }
+  config.rules.c1 = {
+    msg_type: 'text',
+    body: 'TESTCOMPONENT_MSG_TYPE_TEXT',
+    proc: ->(request) {
+      request.build_text_reply(value: 'TESTCOMPONENT_MSG_TYPE_TEXT_callback')
     }
-    config.rules.b2 = {
-      msg_type: 'event',
-      event: ['subscribe', 'scan'],
-      body: /^invite_(member|by)_/,
-      proc: ->(request) { request.reply_for_blank_info }
-    }
-    config.rules.c1 = {
-      msg_type: 'text',
-      body: 'TESTCOMPONENT_MSG_TYPE_TEXT',
-      proc: ->(request) {
-        request.build_text_reply(value: 'TESTCOMPONENT_MSG_TYPE_TEXT_callback')
-      }
-    }
-    config.rules.c2 = {
-      msg_type: 'text',
-      body: /QUERY_AUTH_CODE:/,
-      proc: ->(request) {
-        auth = request.platform.auths.build
-        auth.auth_code = request.body.delete_prefix 'QUERY_AUTH_CODE:'
-        auth.request = request
-        auth.testcase = true
-        auth.save
+  }
+  config.rules.c2 = {
+    msg_type: 'text',
+    body: /QUERY_AUTH_CODE:/,
+    proc: ->(request) {
+      auth = request.platform.auths.build
+      auth.auth_code = request.body.delete_prefix 'QUERY_AUTH_CODE:'
+      auth.request = request
+      auth.testcase = true
+      auth.save
 
-        request.build_success_reply
-      }
+      request.build_success_reply
     }
-  end
-
+  }
 end
