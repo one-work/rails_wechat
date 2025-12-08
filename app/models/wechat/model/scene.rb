af4ae359f71e1e7ef541db9124c0913d80a544ae
qinmingyuan <mingyuan0715@foxmail.com>
2025-12-08 15:48:23 +0800

    moved unused debug

--- app/models/wechat/model/scene.rb
+++ app/models/wechat/model/scene.rb
@@ -82,7 +82,6 @@ module Wechat
 
     def broadcast_to_session
       if broadcast_to.present? && qrcode_url.present?
-        puts '-------------------------'
         SessionChannel.broadcast_to(
           broadcast_to,
           remaining: remaining_seconds,
