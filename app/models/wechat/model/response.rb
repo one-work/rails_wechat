5a642731d6d2c559b38a026a7c7f2ac9a93cc76a
qinmingyuan <mingyuan0715@foxmail.com>
2026-03-06 15:05:18 +0800

    shown replies

--- app/models/wechat/model/response.rb
+++ app/models/wechat/model/response.rb
@@ -11,6 +11,7 @@ module Wechat
 
       belongs_to :app, foreign_key: :appid, primary_key: :appid
       belongs_to :reply, optional: true
+
       has_many :extractors, dependent: :delete_all
       has_many :hooks, dependent: :delete_all
       has_many :request_responses, ->(o){ where(appid: o.appid) }, dependent: :destroy_async
