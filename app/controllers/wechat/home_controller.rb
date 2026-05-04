5387bf59c6897cfd99f566f79d6a65a5d430dd83
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-04 13:09:33 +0800

    fix

--- app/controllers/wechat/home_controller.rb
+++ app/controllers/wechat/home_controller.rb
@@ -1,8 +1,14 @@
 module Wechat
   class HomeController < BaseController
+    after_action :delete_frame_header, only: [:glodplan]
 
     def glodplan
     end
-    
+
+    private
+    def delete_frame_header
+      response.headers.delete('X-Frame-Options')
+    end
+
   end
 end
