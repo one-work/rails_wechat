56033fc0eeea8eee039dabc7c56b8cef2bbe8aa8
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-04 18:00:27 +0800

    small style

--- app/controllers/wechat/home_controller.rb
+++ app/controllers/wechat/home_controller.rb
@@ -5,8 +5,8 @@ module Wechat
     def glodplan
       real_id, _ = params[:out_trade_no].to_s.split('_')
       if real_id
-        order = Trade::Order.find_by(uuid: real_id)
-        @order = url_for(controller: 'trade/my/orders', action: 'show', id: order.id, host: order.organ.host)
+        @order = Trade::Order.find_by(uuid: real_id)
+        @order_url = url_for(controller: 'trade/my/orders', action: 'show', id: @order.id, host: @order.organ.host)
       end
     end
 
