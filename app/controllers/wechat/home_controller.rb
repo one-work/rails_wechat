8f28ac521d5ae664b713970d7a1b409eed812255
qinmingyuan <mingyuan0715@foxmail.com>
2026-05-05 11:34:08 +0800

    support more than one button

--- app/controllers/wechat/home_controller.rb
+++ app/controllers/wechat/home_controller.rb
@@ -6,7 +6,6 @@ module Wechat
       real_id, _ = params[:out_trade_no].to_s.split('_')
       if real_id
         @order = Trade::Order.find_by(uuid: real_id)
-        @order_url = url_for(controller: 'trade/my/orders', action: 'show', id: @order.id, host: @order.organ.host)
       end
     end
 
