ad7011b7b0bdffdb1a58cc8992c34ba6231f8800
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-07 17:38:22 +0800

    fix

--- app/apis/wechat/public_api/template.rb
+++ app/apis/wechat/public_api/template.rb
@@ -6,6 +6,10 @@ class Wechat::PublicApi
       r = get 'template/get_industry', origin: BASE
     end
 
+    def industry_set(industry_id1, industry_id2)
+      post 'template/api_set_industry', industry_id1: industry_id1, industry_id2: industry_id2, origin: BASE
+    end
+
     def templates
       r = get 'template/get_all_private_template', origin: BASE
       r['template_list']
