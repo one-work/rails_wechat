873416067b75b081bdc71cd4f072e9266dd43c0a
qinmingyuan <mingyuan0715@foxmail.com>
2026-06-11 10:10:21 +0800

    identity

--- app/models/wechat/ext/member.rb
+++ app/models/wechat/ext/member.rb
@@ -7,17 +7,13 @@ module Wechat
       attribute :corp_userid, :string
 
       belongs_to :member_inviter, class_name: 'Org::Member', optional: true
-      belongs_to :wechat_user, class_name: 'Wechat::WechatUser', foreign_key: :wechat_openid, primary_key: :uid, optional: true
 
       has_many :corp_users, ->(o) { where(organ_id: o.organ_id) }, class_name: 'Wechat::CorpUser', primary_key: :corp_userid, foreign_key: :userid
-      has_many :wechat_users, class_name: 'Wechat::WechatUser', through: :account, source: :oauth_users
-      has_many :program_users, class_name: 'Wechat::ProgramUser', through: :account, source: :oauth_users
+      has_many :wechat_users, class_name: 'Wechat::WechatUser', primary_key: :identity, foreign_key: :identity
+      has_many :program_users, class_name: 'Wechat::ProgramUser', primary_key: :identity, foreign_key: :identity
       has_many :medias, class_name: 'Wechat::Media'
       has_many :subscribes, -> { where(sending_at: nil) }, class_name: 'Wechat::Subscribe', through: :program_users
       has_many :scenes, as: :handle, class_name: 'Wechat::Scene'
-
-      scope :wechat, -> { where.not(wechat_openid: [nil, '']) }
-      scope :corp, -> { where.not(corp_userid: [nil, '']) }
     end
 
     def invite_member!
