3c5e647162b3b0e1baaf4656b07b8dd6a3af23bc
qinmingyuan <mingyuan0715@foxmail.com>
2026-08-27 13:34:02 +0800

    fix e curerent wechat bug

--- app/controllers/wechat/controller/application.rb
+++ app/controllers/wechat/controller/application.rb
@@ -119,10 +119,10 @@ module Wechat
       if @current_wechat_user
         if request.variant.include?(:mini_program) && current_user
           appid = request.user_agent&.scan(RegexpUtil.between('miniProgram/', '$')).presence || request.referer&.scan(RegexpUtil.between('servicewechat.com/', '/')).presence || Current.session.appid
-          @current_wechat_user.same_oauth_users.where(appid: appid).take
+          @current_wechat_user = @current_wechat_user.same_oauth_users.where(appid: appid).take
         elsif request.variant.include?(:wechat) && current_user
           wechat_appids = (PublicApp.global + PublicApp.default_where(default_ancestors_params)).pluck(:appid).uniq
-          @current_wechat_user.same_oauth_users.where(appid: wechat_appids).take
+          @current_wechat_user = @current_wechat_user.same_oauth_users.where(appid: wechat_appids).take
         end
       end
       logger.debug "\e[35m  Current Wechat User: #{@current_wechat_user&.id}  \e[0m"
