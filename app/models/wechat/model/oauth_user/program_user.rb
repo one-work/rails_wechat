a0abb5ebce16e66f85da2da918dc2ba05a45cbb7
qinmingyuan <mingyuan0715@foxmail.com>
2026-09-13 16:04:14 +0800

    fixed

--- app/models/wechat/model/oauth_user/program_user.rb
+++ app/models/wechat/model/oauth_user/program_user.rb
@@ -9,13 +9,10 @@ module Wechat
 
     def get_phone_number!(params)
       r = Wechat::Cipher.program_decrypt(params[:encryptedData], params[:iv], session_key)
-      logger.debug "\e[35m  Get Phone Number: #{r}  \e[0m"
+      logger.debug "\e[35m  手机号获取: #{r}，session_key 为：#{session_key}  \e[0m"
       if r['phoneNumber']
         self.identity = r['phoneNumber']
         self.save
-      else
-        self.errors.add :base, "手机号获取失败，session_key 为：#{session_key}"
-        nil
       end
     end
 
